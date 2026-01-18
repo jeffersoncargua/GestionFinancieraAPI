1cb8d893b1a390cda8749ff23126346e7db1e36d
Jefferson Cargua <jeffersoncargua@gmail.com>
2026-01-18 18:52:12 -0500

    TNC-6: Se agrego las configuraciones para la base de datos y se configuro la autenticacion de los usarios para utilizar JWT

---
+++ GestionFinancieraNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,53 @@
+﻿using GestionFinancieraNet.Models.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionFinancieraNet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<UserIdentity>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<UserIdentity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
+        /// <summary>
+        /// Este metodo permite colocar los roles que necesitara el sistema, luego de agregarlos a la base de datos, se debe comnetar
+        /// para que no se vuelvan a registrar y ocasione errores a futuro
+        /// </summary>
+        /// <param name="builder"></param>
+        private static void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole()
+                {
+                    Name = "Admin",
+                    ConcurrencyStamp = "1",
+                    NormalizedName = "ADMIN",
+                },
+                new IdentityRole()
+                {
+                    Name = "Collab",
+                    ConcurrencyStamp = "2",
+                    NormalizedName = "COLLAB",
+                },
+                new IdentityRole()
+                {
+                    Name = "Costumer",
+                    ConcurrencyStamp = "3",
+                    NormalizedName = "COSTUMER",
+                }
+            );
+        }
+    }
+
+}
---
+++ GestionFinancieraNet/Models/Entity/UserIdentity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GestionFinancieraNet.Models.Entity
+{
+    public class UserIdentity : IdentityUser
+    {
+        public required string Phone { get; set; }
+        public required string Location { get; set; }
+    }
+}
